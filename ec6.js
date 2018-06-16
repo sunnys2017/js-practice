88e503fc2d70a13e12d4aede82dbda5d98d3b385
sunny <sunnysun2108@gmail.com>
2018-06-16 17:58:19 -0400

    object literals and block scope

--- ec6.js
+++ ec6.js
@@ -112,8 +112,123 @@ new Shouter("Dr. Loudmouth").speak("hello there")
 //Dr. Loudmouth says 'hello there'
 //Dr. Loudmouth shorts 'HELLO THERE'
 
-/*
+/*Object literals.1:
 Fake point
 Use a single object literal to create an object that is indistinguishable 
 from a Point instance, without calling the Point constructor.
 */
+
+
+/*Object literals.2:
+Configurable property
+Fill in the startNode function using a single object literal. The function 
+should return an object with type and value properties containing the value of 
+the arguments by those names, and a third property, named by the sourceProperty 
+option, set to the value of the sourceValue option.
+*/
+function startNode(type, value, options) {
+  return {//your code here
+    type,
+    value,
+    src: options.sourceValue
+  }
+}
+
+console.log(startNode("Identifier", "foo", {
+  sourceProperty: "src",
+  sourceValue: "bar.js"
+}))
+// → {type: "Identifier",
+//    value: "foo",
+//    src: "bar.js"}
+
+/*Object literals.3:
+Singleton
+Add a get method to the ids object, which returns the next id and increments 
+its next counter. Use the short method syntax.
+*/
+var ids = {
+  next: 0, //your code here
+  get(){
+    return this.next++
+  }
+}
+
+console.log(ids.get())
+// → 0
+console.log(ids.get())
+// → 1
+
+/*Block scope.1:
+Closing over scope
+This is a typical mistake to make in JavaScript. We create a number of functions in 
+ loop, and refer to an outside variable from these functions. All of them will end
+  up referring to the same variable, which ends up being incremented to 10. Thus, 
+  callbacks[2] does not log 2. It logs 10, as do all functions in the array.
+Do you know the solution for such situations in ES5? Does ES6 provide a cleaner 
+solution?
+var callbacks = []
+for (var i = 0; i < 10; i++) {
+  callbacks.push(function() { console.log(i) })
+}
+callbacks[2]()
+*/
+var callbacks = []
+for (let i = 0; i < 10; i++) {
+  callbacks.push(function() { console.log(i) })
+}
+callbacks[2]()
+
+/*
+Block scope.2:
+Constant non-constance
+Does the fact that account is constant mean that we can't update password? 
+Why, or why not? And if not, how could we make it so that we can't?
+const account = {
+  username: "marijn",
+  password: "xyzzy"
+}
+account.password = "s3cret" // (*much* more secure)
+console.log(account.password)
+this only makes the variable itself immutable, not its assigned content 
+(for instance, in case the content is an object, this means the object 
+itself can still be altered).
+Use Object.freeze() to make object immutable
+*/
+const account = {
+  username: "marijn",
+  password: "xyzzy"
+}
+Object.freeze(account) //my code here
+account.password = "s3cret" // (*much* more secure)
+
+console.log(account.password)
+
+/*Block scope.3:
+Hoist the class
+This code produces an error. So apparently, unlike functions, classes are 
+not hoisted to the top of their scope. (They are block-scoped.)
+What could be the reason for this?
+let s = new Something()
+class Something {}
+*/
+
+/*
+The difference between var/function/function* declarations and let/const/class 
+declara­tions is the initialisation.
+The former are initialised with undefined or the (generator) function right 
+when the binding is created at the top of the scope. The lexically declared 
+variables however stay uninitialised. This means that a ReferenceError exception
+ is thrown when you try to access it. It will only get initialised when the 
+ let/const/class statement is evaluated, everything before (above) that is called
+  the temporal dead zone.
+*/
+
+/*
+Arrow functions.1:
+Accounting
+Write an expression using higher-order array methods (say, filter and reduce) to 
+compute the total value of the machines in the inventory array.
+*/
+
+
